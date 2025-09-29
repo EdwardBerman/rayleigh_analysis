35e5a47d29a0a5826a80adebcf60a224af375096
EdwardBerman <10emb29@gmail.com>
2025-09-28 22:22:48 -0400

    load all of the lrgb

--- data_preprocessing/longe_range_graph_benchmark.py
+++ data_preprocessing/longe_range_graph_benchmark.py
@@ -2,16 +2,49 @@ from torch_geometric.datasets import LRGBDataset
 from torch_geometric.loader import DataLoader
 
 root = "data/LRGB"
-dataset_name = "PascalVOC-SP"  # e.g. one of "PascalVOC-SP", "COCO-SP", "PCQM-Contact", "Peptides-func", "Peptides-struct"
+dataset_pascal = "PascalVOC-SP"  # e.g. one of "PascalVOC-SP", "COCO-SP", "PCQM-Contact", "Peptides-func", "Peptides-struct"
+dataset_coco = "COCO-SP"
+dataset_PCQM = "PCQM-Contact"
+dataset_peptides_func = "Peptides-func"
+dataset_peptides_struct = "Peptides-struct"
 
-# 2. Create the dataset object
+dataset_name = dataset_pascal  
 dataset = LRGBDataset(root=root, name=dataset_name, split="train")
 
 loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
 for data in loader:
-    # data is a PyG Data object (or a batch of them)
-    # e.g. data.x, data.edge_index, data.y, etc.
+    print(data)
+    break
+
+dataset_name = dataset_coco  
+dataset = LRGBDataset(root=root, name=dataset_name, split="train")
+
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+for data in loader:
+    print(data)
+    break
+
+dataset_name = dataset_PCQM  
+dataset = LRGBDataset(root=root, name=dataset_name, split="train")
+
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+for data in loader:
+    print(data)
+    break
+
+dataset_name = dataset_peptides_func  
+dataset = LRGBDataset(root=root, name=dataset_name, split="train")
+
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+for data in loader:
+    print(data)
+    break
+
+dataset_name = dataset_peptides_struct  
+dataset = LRGBDataset(root=root, name=dataset_name, split="train")
+
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+for data in loader:
     print(data)
     break
 
