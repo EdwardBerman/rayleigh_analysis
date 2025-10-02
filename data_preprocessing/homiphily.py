aa96853d1833dc0e9ce9e676b205907e67d324f4
EdwardBerman <10emb29@gmail.com>
2025-10-01 21:37:42 -0400

    dataset investigation

---
+++ data_preprocessing/homiphily.py
@@ -0,0 +1,16 @@
+from external.homiphily import our_measure
+from parsers.parser_lrgb import LongeRangeGraphBenchmarkParser
+
+if __name__ == "__main__":
+    parser = LongeRangeGraphBenchmarkParser(name="PascalVOC-SP", verbose=True)
+    train_dataset, val_dataset, test_dataset = parser.return_datasets()
+    full_dataset = ConcatDataset([train_dataset, val_dataset, test_dataset])
+    edge_indices = full_dataset.data.edge_index
+    node_labels = full_dataset.data.y
+    print("Homophily PascalVOC-SP:", our_measure(edge_indices, node_labels))
+    parser = LongeRangeGraphBenchmarkParser(name="COCO-SP", verbose=True)
+    train_dataset, val_dataset, test_dataset = parser.return_datasets()
+    full_dataset = ConcatDataset([train_dataset, val_dataset, test_dataset])
+    edge_indices = full_dataset.data.edge_index
+    node_labels = full_dataset.data.y
+    print("Homophily COCO-SP:", our_measure(edge_indices, node_labels))
