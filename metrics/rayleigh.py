f583b479c6f6bd6aab8471e0b6d0d95f8c601dd5
EdwardBerman <10emb29@gmail.com>
2025-10-12 13:13:26 -0400

    update to relu

--- metrics/rayleigh.py
+++ metrics/rayleigh.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch_geometric.data import Data
 from torch_geometric.utils import to_dense_adj, to_torch_coo_tensor
 
-
-# TODO: Make these more mem efficient
 def rayleigh_error(f: nn.Module, X: Data) -> torch.Tensor:
     """ 
         Computes |R_G(f(X; A)) - R_G(X; A)| 
@@ -27,7 +26,7 @@ def rayleigh_error(f: nn.Module, X: Data) -> torch.Tensor:
     rayleigh_X_prime = torch.trace(
         X_prime.T @ A_tilde @ X_prime) / torch.norm(X_prime, p='fro')
 
-    return torch.abs(rayleigh_X - rayleigh_X_prime)
+    return F.relu(rayleigh_X - rayleigh_X_prime)
 
 
 def integrated_rayleigh_error(f: nn.Module, X: Data) -> torch.Tensor:
