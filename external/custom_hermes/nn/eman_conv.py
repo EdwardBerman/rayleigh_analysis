b1388fe5377dfa66752b36ac6b4ac5246e4d8915
EdwardBerman <10emb29@gmail.com>
2025-11-04 23:19:09 -0500

    scatter

--- external/custom_hermes/nn/eman_conv.py
+++ external/custom_hermes/nn/eman_conv.py
@@ -8,7 +8,7 @@ from torch.utils.checkpoint import checkpoint
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.inits import glorot, zeros
 from torch_geometric.utils import softmax
-from torch_scatter import scatter_add
+from external.alt_scatter import scatter_add
 
 from external.hermes.src.utils.einsum import einsum
 from external.hermes.src.utils.kernel import build_kernel, build_self_kernel
