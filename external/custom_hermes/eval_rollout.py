3aaf3ead31e56341cb4a7ce7090af9e97790b0e5
EdwardBerman <10emb29@gmail.com>
2025-11-04 22:40:42 -0500

    make look nicer

--- external/custom_hermes/eval_rollout.py
+++ external/custom_hermes/eval_rollout.py
@@ -15,6 +15,42 @@ from external.custom_hermes.utils import create_dataset_loaders
 
 import matplotlib.pyplot as plt
 
+def set_rc_params(fontsize=None):
+    '''
+    Set figure parameters
+    '''
+
+    if fontsize is None:
+        fontsize = 16
+    else:
+        fontsize = int(fontsize)
+
+    rc('font', **{'family': 'serif'})
+    rc('text', usetex=False)
+
+    plt.rcParams.update({'axes.linewidth': 1.3})
+    plt.rcParams.update({'xtick.labelsize': fontsize})
+    plt.rcParams.update({'ytick.labelsize': fontsize})
+    plt.rcParams.update({'xtick.major.size': 8})
+    plt.rcParams.update({'xtick.major.width': 1.3})
+    plt.rcParams.update({'xtick.minor.visible': True})
+    plt.rcParams.update({'xtick.minor.width': 1.})
+    plt.rcParams.update({'xtick.minor.size': 6})
+    plt.rcParams.update({'xtick.direction': 'out'})
+    plt.rcParams.update({'ytick.major.width': 1.3})
+    plt.rcParams.update({'ytick.major.size': 8})
+    plt.rcParams.update({'ytick.minor.visible': True})
+    plt.rcParams.update({'ytick.minor.width': 1.})
+    plt.rcParams.update({'ytick.minor.size': 6})
+    plt.rcParams.update({'ytick.direction': 'out'})
+    plt.rcParams.update({'axes.labelsize': fontsize})
+    plt.rcParams.update({'axes.titlesize': fontsize})
+    plt.rcParams.update({'legend.fontsize': int(fontsize-2)})
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['text.latex.preamble'] = r'\usepackage{amssymb}'
+
+set_rc_params(20)
+
 objects = {
     "armadillo": examples.download_armadillo(),
     "bunny_coarse": examples.download_bunny_coarse(),
