6715d5d93a466e3ce3956b56d6b8f9d6a15e9175
EdwardBerman <10emb29@gmail.com>
2025-11-07 16:05:08 -0500

    fix

--- model/model_factory.py
+++ model/model_factory.py
@@ -137,7 +137,7 @@ def build_model(node_dim: int,
             for layer in range(num_layers):
                 input_dim = node_dim if layer == 0 else hidden_size
                 output_dim = node_dim if layer == num_layers - 1 else hidden_size
-                if input_dim == output_dim:
+                if input_dim != output_dim:
                     print(f"Warning: For Lie Unitary GCN, input and output dimensions must be the same, but a distinct hidden size was set. \nSetting output dim {output_dim} to be input dim {input_dim}\nDid you mean Seperable Unitary Convolution?")
                     output_dim = input_dim
                 module_list.append(UnitaryGCNConvLayer(input_dim,
