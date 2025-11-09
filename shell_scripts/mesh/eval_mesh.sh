e3a64991e9774f22255fab58c72d53cc6269a31a
EdwardBerman <10emb29@gmail.com>
2025-11-09 13:32:00 -0500

    gcn

--- shell_scripts/mesh/eval_mesh.sh
+++ shell_scripts/mesh/eval_mesh.sh
@@ -6,3 +6,5 @@ python3 -m external.custom_hermes.eval_rollout dataset=wave backbone=hermes mode
 python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=eman model_save_path=external/hermes/pretrained_checkpoints/Heat_EMAN_model.pt
 python3 -m external.custom_hermes.eval_rollout dataset=wave backbone=eman model_save_path=external/hermes/pretrained_checkpoints/Wave_EMAN_model.pt
 
+python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=gem_cnn model_save_path=external/hermes/pretrained_checkpoints/Heat_GemCNN_model.pt
+python3 -m external.custom_hermes.eval_rollout dataset=wave backbone=gem_cnn model_save_path=external/hermes/pretrained_checkpoints/Wave_GemCNN_model.pt
