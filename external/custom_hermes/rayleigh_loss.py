87ed50c443e3b08378fb7341b7e4ab378e4dbb4a
EdwardBerman <10emb29@gmail.com>
2025-11-09 01:35:30 -0500

    ucstom loss

---
+++ external/custom_hermes/rayleigh_loss.py
@@ -0,0 +1,28 @@
+import torch
+from torch_geometric.utils import degree
+from torch_geometric.data import Data
+
+def evaluate_rayleigh_loss(y_true: torch.Tensor, y_pred: torch.Tensor, edge_index: torch.Tensor) -> float:
+    src, dst = edge_index[0], edge_index[1]
+    N = y_true.size(0)
+    deg_in = degree(dst, num_nodes=N, dtype=values.dtype).clamp(min=1.0)
+    inv_sqrt_deg = deg_in.rsqrt().view(N, 1)
+
+    def norm_sqrt_deg(x: torch.Tensor) -> torch.Tensor:
+        return x * inv_sqrt_deg
+
+    traj_true_rq = []
+    traj_pred_rq = []
+
+    y_norm      = norm_sqrt_deg(y)       
+    y_pred_norm = norm_sqrt_deg(y_pred)  
+    diff_true = y_norm[src, 0] - y_norm[dst, 0]         
+    diff_pred = y_pred_norm[src, 0] - y_pred_norm[dst, 0]
+    edge_mse_true = (diff_true ** 2).sum()
+    edge_mse_pred = (diff_pred ** 2).sum()
+
+    sum_nodes_sq_gt = y.pow(2).sum()
+    sum_nodes_sq_pred = y_pred.pow(2).sum()
+
+    return (traj_true_rq.append(edge_mse_true.item()*0.5/(sum_nodes_sq_gt.item()+1e-16)) - traj_pred_rq.append(edge_mse_pred.item()*0.5/(sum_nodes_sq_pred.item()+1e-16)))**2 + torch.nn.MSELoss(y_true, y_pred)
+
