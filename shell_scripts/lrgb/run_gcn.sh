4362a788a2c24caaa6a910ac5326eb40a4b61818
EdwardBerman <10emb29@gmail.com>
2025-11-09 18:49:00 -0500

    shell scripts

---
+++ shell_scripts/lrgb/run_gcn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python3 -m train.train \
+  --dataset COCO-SP \
+  --architecture GCN \
+  --num_layers 8 \
+  --activation_function ReLU \
+  --batch_size 64 \
+  --batch_norm None \
+  --num_attention_heads 2 \
+  --dropout_rate 0.1 \
+  --hidden_size 128 \
+  --edge_aggregator NONE \
+  --optimizer Muon \
+  --lr 0.001 \
+  --epochs 200 \
+  --weight_decay 0.01 \
+  --window_size 4 \
+  --receptive_field 5 \
+  --save_dir output \
+  --log_rq
