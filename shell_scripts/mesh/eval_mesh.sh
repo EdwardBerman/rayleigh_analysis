325ff25ac4573fdce95d6dca9db7260671e6b0a6
EdwardBerman <10emb29@gmail.com>
2025-11-11 12:12:30 -0500

    John didn't give a script to generate CH experiment

--- shell_scripts/mesh/eval_mesh.sh
+++ shell_scripts/mesh/eval_mesh.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-python3 -m external.custom_hermes.eval_rollout dataset=cahn_hilliard backbone=hermes model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_Hermes_model.pt
-python3 -m external.custom_hermes.eval_rollout dataset=cahn_hilliard backbone=eman model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_EMAN_model.pt
-python3 -m external.custom_hermes.eval_rollout dataset=cahn_hilliard backbone=gem_cnn model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_GemCNN_model.pt
-
 python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=hermes model_save_path=external/hermes/pretrained_checkpoints/Heat_Hermes_model.pt
 python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=eman model_save_path=external/hermes/pretrained_checkpoints/Heat_EMAN_model.pt
 python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=gem_cnn model_save_path=external/hermes/pretrained_checkpoints/Heat_GemCNN_model.pt
