e6de4b3df5354ed968853ca650a819c7e73a4aec
EdwardBerman <10emb29@gmail.com>
2025-11-12 17:17:39 -0500

    save x and y too

--- metrics/heat_flow.py
+++ metrics/heat_flow.py
@@ -35,12 +35,16 @@ def evaluate_heat_flow(model, loader, device):
 @torch.no_grad()
 def rayleigh_quotient_distribution(model, loader, device, save_dir):
     model.eval()
+    rayleigh_quotients_x = []
     rayleigh_quotients_xprime = []
+    rayleigh_quotients_y = []
 
     for data in loader:
         data = data.to(device)
-        _, xprime, _ = rayleigh_quotients(model, data)
+        x, xprime, y = rayleigh_quotients(model, data)
+        rayleigh_quotients_x.append(x.item())
         rayleigh_quotients_xprime.append(xprime.item())
+        rayleigh_quotients_y.append(y.item())
 
     rayleigh_quotients_xprime = np.array(rayleigh_quotients_xprime)
     plt.hist(rayleigh_quotients_xprime, bins=30, density=True)
@@ -48,7 +52,10 @@ def rayleigh_quotient_distribution(model, loader, device, save_dir):
     plt.ylabel("Density")
     plt.title("Distribution of Rayleigh Quotients")
     plt.savefig(os.path.join(save_dir, "rayleigh_quotient_distribution.png"))
+    plt.close()
+    np.save(os.path.join(save_dir, "rayleigh_quotients_x.npy"), rayleigh_quotients_x)
     np.save(os.path.join(save_dir, "rayleigh_quotients_xprime.npy"), rayleigh_quotients_xprime)
+    np.save(os.path.join(save_dir, "rayleigh_quotients_y.npy"), rayleigh_quotients_y)
 
 
 
