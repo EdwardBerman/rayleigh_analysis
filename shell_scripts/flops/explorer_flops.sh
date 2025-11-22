c44c648e07988b111a6c6194544e4317bda7b79f
EdwardBerman <10emb29@gmail.com>
2025-11-21 22:34:06 -0500

    flops

--- shell_scripts/flops/explorer_flops.sh
+++ shell_scripts/flops/explorer_flops.sh
@@ -17,5 +17,3 @@ eval "$(poetry env activate)"
 
 
 python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
-  --verbose --act ReLU --edge_agg GINE \
-  --project uni_gine_relu
--- shell_scripts/flops/flops.sh
+++ shell_scripts/flops/flops.sh
@@ -2,6 +2,4 @@
 
 
 python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
-    --verbose --act ReLU --edge_agg GINE \
-    --project uni_gine_relu
 
