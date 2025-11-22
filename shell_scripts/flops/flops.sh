cefa4f7832b4154fc6079f5959a2c31bf7f49adf
EdwardBerman <10emb29@gmail.com>
2025-11-21 22:31:17 -0500

    flops

--- shell_scripts/flops/flops.sh
+++ shell_scripts/flops/flops.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --time=07:59:59
-#SBATCH --job-name=[h200train]
-#SBATCH --mem=32GB
-#SBATCH --ntasks=1
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:h200
-#SBATCH --output=slurm/%j.out
-#SBATCH --error=slurm/%j.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=berman.ed@northeastern.edu
-
-module load python/3.13.5
-
-eval "$(poetry env activate)"
 
 
 for trunc in 1 2 4 8 16 32 64 128
