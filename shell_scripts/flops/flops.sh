d9398b1887afdba97fa219312b1aaadc308cc9bd
EdwardBerman <10emb29@gmail.com>
2025-11-21 22:29:17 -0500

    .

---
+++ shell_scripts/flops/flops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=07:59:59
+#SBATCH --job-name=[h200train]
+#SBATCH --mem=32GB
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:h200
+#SBATCH --output=slurm/%j.out
+#SBATCH --error=slurm/%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=berman.ed@northeastern.edu
+
+module load python/3.13.5
+
+eval "$(poetry env activate)"
+
+
+for trunc in 1 2 4 8 16 32 64 128
+do
+    echo "Running with truncation=$trunc"
+    python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
+        --truncation $trunc \
+        --verbose --act ReLU --edge_agg GINE \
+        --project uni_gine_relu
+done
+
