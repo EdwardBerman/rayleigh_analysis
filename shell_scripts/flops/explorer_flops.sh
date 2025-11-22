df60ac84b523ace98ec62333cad714ea41177747
EdwardBerman <10emb29@gmail.com>
2025-11-21 22:32:21 -0500

    flops

--- shell_scripts/flops/explorer_flops.sh
+++ shell_scripts/flops/explorer_flops.sh
@@ -16,12 +16,7 @@ module load python/3.13.5
 eval "$(poetry env activate)"
 
 
-for trunc in 1 2 4 8 16 32 64 128
-do
-    echo "Running with truncation=$trunc"
-    python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
-        --truncation $trunc \
-        --verbose --act ReLU --edge_agg GINE \
-        --project uni_gine_relu
-done
-
+python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
+  --truncation $trunc \
+  --verbose --act ReLU --edge_agg GINE \
+  --project uni_gine_relu
--- shell_scripts/flops/flops.sh
+++ shell_scripts/flops/flops.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 
-for trunc in 1 2 4 8 16 32 64 128
-do
-    echo "Running with truncation=$trunc"
-    python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
-        --truncation $trunc \
-        --verbose --act ReLU --edge_agg GINE \
-        --project uni_gine_relu
-done
+python3 -m flops.flops_counter --architecture Uni --data_dir toy_heat_diffusion/data
+    --truncation $trunc \
+    --verbose --act ReLU --edge_agg GINE \
+    --project uni_gine_relu
 
