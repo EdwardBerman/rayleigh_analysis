18b0de818e6a085f680df2c7239548abdc3dec1f
EdwardBerman <10emb29@gmail.com>
2025-11-27 01:48:50 -0500

    robust laplacian

--- external/custom_hermes/eval_rollout.py
+++ external/custom_hermes/eval_rollout.py
@@ -136,8 +136,8 @@ def main(cfg):
             L, M = robust_laplacian.mesh_laplacian(pos.cpu().numpy(), face.T.cpu().numpy())
             print("Computed robust Laplacian")
             # verify L symmetric
-            L_np = L.toarray()
-            print("L symmetric:", np.allclose(L, L.T))
+            L_torch = torch.from_numpy(L)
+            print("L symmetric:", torch.allclose(L_torch, L_torch.T, atol=1e-6))
 
 
             all_preds = []
