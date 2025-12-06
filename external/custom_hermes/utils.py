360dd2ab3c8923c28035b49e23aecc3e33a4134d
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-12-06 00:06:02 -0800

    luisa did not bother writing a commit message

--- external/custom_hermes/utils.py
+++ external/custom_hermes/utils.py
@@ -91,7 +91,7 @@ def create_dataset_loaders(cfg, return_datasets=False):
 
         else:
             out_dict[split] = DataLoader(
-                dataset,
+                dataset[:10],
                 batch_size=cfg.train.batch_size,
                 shuffle=train,
                 pin_memory=True,
