12d8c1e9c91dd46c918d5d600049565353e1e78f
EdwardBerman <10emb29@gmail.com>
2025-12-07 02:46:31 -0500

    progress?

--- external/custom_hermes/backbone/mesh_transformer.py
+++ external/custom_hermes/backbone/mesh_transformer.py
@@ -166,7 +166,11 @@ class GraphViT(nn.Module):
             output_hat.append(next_output)
 
         output_hat = torch.stack(output_hat, dim=1)
-        output_hat = output_hat.squeeze(0)
+        print("Output hat shape:", output_hat.shape)
+        breakpoint()
+        output_hat = output_hat.squeeze(0).permute(1, 0, 2)
+        print("Output hat permuted shape:", output_hat.shape)
+        breakpoint()
         return output_hat
 
 
