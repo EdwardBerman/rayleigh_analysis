b8209869d03ab8b371c4e894cc86889316b9dac9
EdwardBerman <10emb29@gmail.com>
2025-12-07 02:54:09 -0500

    transformer

---
+++ shell_scripts/mesh/explorer_transformer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=07:59:59
+#SBATCH --job-name=[h200train]
+#SBATCH --mem=32GB
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:h200
+#SBATCH --output=slurm/%j.out
+#SBATCH --error=slurm/%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=berman.ed@northeastern.edu
+
+module load python/3.13.5
+
+eval "$(poetry env activate)"
+
+
+python3 -m external.custom_hermes.train dataset=heat backbone=transformer
