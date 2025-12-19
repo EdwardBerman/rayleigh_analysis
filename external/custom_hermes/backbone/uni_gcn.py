2b537dd5662c4e4b8c05a4f01f4571760cc6213a
EdwardBerman <10emb29@gmail.com>
2025-12-18 22:43:56 -0500

    only use heat feature

--- external/custom_hermes/backbone/uni_gcn.py
+++ external/custom_hermes/backbone/uni_gcn.py
@@ -48,7 +48,7 @@ class Uni(nn.Module):
         for transform in self.transforms:
             data = transform(data)
 
-        x = data.x[:, -1:]
+        x = data.x[:, 3:4]
 
         # Setting the features of isolated nodes to 0
         if self.null_isolated:
