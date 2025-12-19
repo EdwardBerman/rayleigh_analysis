e03b704531f541c4f70e5c738b9989f44d0656a4
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-12-19 17:08:00 -0500

    rah

---
+++ weatherbench/preprocess_data.py
@@ -0,0 +1,5 @@
+"""
+Downloads and preprocesses data from Weatherbench.
+Note that this is *not* the most efficient way to do this, likely the optimal way involves streaming directly from the Google Buckets. 
+For the purposes of this research we are only using a somewhat small subset of data, and thus download and preprocess things locally for speed.
+"""
