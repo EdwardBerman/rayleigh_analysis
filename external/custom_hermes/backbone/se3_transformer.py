5e386e898f2b02e2ed50c5851663c7d258c0858c
EdwardBerman <10emb29@gmail.com>
2025-12-26 23:24:19 -0500

    shrink radial

--- external/custom_hermes/backbone/se3_transformer.py
+++ external/custom_hermes/backbone/se3_transformer.py
@@ -1,18 +1,14 @@
 from se3_transformer_pytorch import SE3Transformer as BaseSE3Transformer
 from torch_geometric.utils import to_dense_adj
 
-
 class SE3Transformer(BaseSE3Transformer):
+    def __init__(self, *args, radial_hidden=32, **kwargs):  # Add radial_hidden param
+        # Override the default radial_hidden (128) with a smaller value
+        super().__init__(*args, radial_hidden=radial_hidden, **kwargs)
+    
     def forward(self, data):
-        # Shape into [b, n, d, 1]
         x = data.x.unsqueeze(0).squeeze(-1)
-
-        # Shape into [b, n, d]
         pos = data.pos.unsqueeze(0)
-
-        # Create mask from edge indices
         adj_mat = to_dense_adj(data.edge_index)
-
         out = super().forward(x, pos, adj_mat=adj_mat)
-
         return out.squeeze(0).unsqueeze(-1)
