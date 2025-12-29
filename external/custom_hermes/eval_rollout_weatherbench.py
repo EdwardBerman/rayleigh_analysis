f59e43b6c96c2f82d4bf2105d970c1d3634d4dfa
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-12-29 10:34:36 -0500

    luisa did not bother writing a commit message

--- external/custom_hermes/eval_rollout_weatherbench.py
+++ external/custom_hermes/eval_rollout_weatherbench.py
@@ -57,11 +57,12 @@ def main(cfg):
         T = dataset.rollout_steps
         level = dataset.level
         pred_timedelta = (np.arange(1, T + 1) * 6).astype("timedelta64[h]")
-        zarr_path = Path(cfg.save_dir) / "wb_predictions.zarr"
+        zarr_path = Path(cfg.save_dir) / "fivetrajectories"
         first_write = True
 
         model.eval()
-        for idx in range(dataset.num_trajectories()):
+        # for idx in range(dataset.num_trajectories()):
+        for idx in range(5):
             data = dataset.get_trajectory(idx)
             data = data.to(cfg.device)
 
