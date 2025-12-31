5626f3fdbaa6a367f3dec71a636874c3b9421502
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-12-31 11:45:48 -0500

    making uni gcn less hardcoded

--- external/custom_hermes/backbone/uni_gcn.py
+++ external/custom_hermes/backbone/uni_gcn.py
@@ -4,12 +4,16 @@ from torch import nn
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils import remove_self_loops
 
-from external.ortho_gcn import OrthogonalGCNConvLayer, GroupSort
+from external.ortho_gcn import GroupSort, OrthogonalGCNConvLayer
 
 
 class Uni(nn.Module):
     def __init__(
         self,
+        input_dim: int,
+        hidden_dim: int,
+        num_layers: int,
+        encoder: nn.Module | None,  # if None, will pad.
         null_isolated,
         add_self_loops,
         dropout,
@@ -18,32 +22,31 @@ class Uni(nn.Module):
     ):
         super().__init__()
 
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
         self.null_isolated = null_isolated
         self.add_self_loops = add_self_loops
         self.dropout = dropout
         self.final_activation = final_activation
 
-        block_kwargs = dict(
-            add_self_loops=add_self_loops, dropout=dropout
-        )
-
         self.transforms = []
 
         self.blocks = nn.ModuleList()
 
-        for i in range(12):
-            if i == 11:
+        for i in range(num_layers):
+            if i == num_layers - 1:
                 self.blocks.append(
-                    GCNConv(64, 1, add_self_loops=add_self_loops)
+                    GCNConv(self.hidden_dim, self.input_dim,
+                            add_self_loops=add_self_loops)
                 )
             else:
                 self.blocks.append(
-                    OrthogonalGCNConvLayer(64,
-                                           64,
+                    OrthogonalGCNConvLayer(self.hidden_dim,
+                                           self.hidden_dim,
                                            dropout=dropout,
                                            residual=False,
                                            global_bias=False,
-                                           T=10,
+                                           T=T,
                                            use_hermitian=True,
                                            activation=GroupSort)
 
@@ -57,9 +60,10 @@ class Uni(nn.Module):
 
         if x.dim() == 1:
             x = x.unsqueeze(-1)
-        
-        padding_size = 64 - x.shape[-1]
-        x = torch.nn.functional.pad(x, (0, padding_size), mode='constant', value=0)
+
+        padding_size = self.hidden_dim - x.shape[-1]
+        x = torch.nn.functional.pad(
+            x, (0, padding_size), mode='constant', value=0)
 
         # check if object has "rewired" attribute
         if not hasattr(data, 'rewired') or not data.rewired:
@@ -72,7 +76,8 @@ class Uni(nn.Module):
 
                 row = torch.from_numpy(L.row).long().to(data.x.device)
                 col = torch.from_numpy(L.col).long().to(data.x.device)
-                val = torch.from_numpy(L.data).to(data.x.device).to(data.x.dtype)
+                val = torch.from_numpy(L.data).to(
+                    data.x.device).to(data.x.dtype)
                 mask = row != col
                 row, col, val = row[mask], col[mask], val[mask]
                 edge_index = torch.stack([row, col], dim=0).contiguous()
@@ -95,7 +100,7 @@ class Uni(nn.Module):
         input_data_obj.edge_weight = edge_weight
 
         for i, block in enumerate(self.blocks):
-            if i == 11:
+            if i == len(self.blocks) - 1:
                 x = block(x, edge_index, edge_weight)
             else:  # OrthogonalGCNConvLayer
                 input_data_obj.x = x
