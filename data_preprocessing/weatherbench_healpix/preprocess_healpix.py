84be2179b282d2eea93c1fee00a4168754815e16
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-03-23 22:24:29 -0400

    luisa did not bother writing a commit message

--- data_preprocessing/weatherbench_healpix/preprocess_healpix.py
+++ data_preprocessing/weatherbench_healpix/preprocess_healpix.py
@@ -6,36 +6,18 @@ mesh_path = "./data/weatherbench/earth_mesh.vtp"
 t850_savepath = "data/weatherbench_healpix/preprocessed/t850"
 z500_savepath = 'data/weatherbench_healpix/preprocessed/z500'
 
-# WeatherbenchHealpix.preprocess_and_save(
-#     era5_path=era5_path,
-#     save_dir=t850_savepath,
-#     task="t850",
-#     nside=32,
-#     lmax=20,
-# )
-
-# WeatherbenchHealpix.preprocess_and_save(
-#     era5_path=era5_path,
-#     save_dir=z500_savepath,
-#     task="z500",
-#     nside=32,
-#     lmax=20
-# )
-
-train850 = WeatherbenchHealpix.from_cache(
+WeatherbenchHealpix.preprocess_and_save(
     era5_path=era5_path,
-    mesh_path=mesh_path,
-    cache_dir="data/weatherbench_healpix/preprocessed/t850",
-    split="train",
+    save_dir=t850_savepath,
     task="t850",
+    nside=32,
+    lmax=20,
 )
 
-test850 = WeatherbenchHealpix.from_cache(
+WeatherbenchHealpix.preprocess_and_save(
     era5_path=era5_path,
-    mesh_path=mesh_path,
-    cache_dir="data/weatherbench_healpix/preprocessed/t850",
-    split="test",
-    task="t850",
+    save_dir=z500_savepath,
+    task="z500",
+    nside=32,
+    lmax=20
 )
-
-breakpoint()
