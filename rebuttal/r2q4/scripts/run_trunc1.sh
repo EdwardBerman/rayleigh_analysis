e7ff0f36b5f3f76f1c5792c3bd589822e07c3e80
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-03-25 16:08:22 -0400

    luisa did not bother writing a commit message

--- rebuttal/r2q4/scripts/run_trunc1.sh
+++ rebuttal/r2q4/scripts/run_trunc1.sh
@@ -12,39 +12,6 @@
 module load python/3.13.5
 
 eval "$(poetry env activate)"
-python3 -m toy_heat_diffusion.train \
-    --data_dir toy_heat_diffusion/data \
-    --train_steps 3 \
-    --eval_steps 1 \
-    --model lie_unitary \
-    --layers 12 \
-    --act Identity \
-    --hidden 1 \
-    --epochs 200 \
-    --lr 0.0001 \
-    --batch_size 64 \
-    --dropout 0.0 \
-    --truncation_level 1 \
-    --save_dir rebuttal/r2q4/lie_unitary_trunc1_trial0 \
-    --entity_name rayleigh_analysis_gnn \
-    --project_name truncation_rebuttal
-
-python3 -m toy_heat_diffusion.train \
-    --data_dir toy_heat_diffusion/data \
-    --train_steps 3 \
-    --eval_steps 1 \
-    --model lie_unitary \
-    --layers 12 \
-    --act Identity \
-    --hidden 1 \
-    --epochs 200 \
-    --lr 0.0001 \
-    --batch_size 64 \
-    --dropout 0.0 \
-    --truncation_level 1 \
-    --save_dir rebuttal/r2q4/lie_unitary_trunc1_trial1 \
-    --entity_name rayleigh_analysis_gnn \
-    --project_name truncation_rebuttal
 
 python3 -m toy_heat_diffusion.train \
     --data_dir toy_heat_diffusion/data \
