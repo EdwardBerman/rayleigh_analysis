5e7ed20d409f31a9ebe9c3e77e8a813cc1dbc93b
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-03-28 17:32:03 -0400

    luisa did not bother writing a commit message

---
+++ rebuttal/dynamic_tmax/eval_all.sh
@@ -0,0 +1,6 @@
+
+python3 -m external.custom_hermes.eval_rollout dataset=cahn_hilliard backbone=uni model_save_path=model_checkpoints/adaptiveuni/Cahn-Hilliard_Uni_seed1_model.pt
+
+python3 -m external.custom_hermes.eval_rollout dataset=heat backbone=uni model_save_path=model_checkpoints/adaptiveuni/Heat_Uni_seed1_model.pt
+
+python3 -m external.custom_hermes.eval_rollout dataset=wave backbone=uni model_save_path=model_checkpoints/adaptiveuni/Wave_Uni_seed1_model.pt
