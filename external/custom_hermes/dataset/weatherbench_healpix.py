8250283b66fdad1501fd5cf862137b278fc9bbbd
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-03-28 22:44:21 -0400

    luisa did not bother writing a commit message

--- external/custom_hermes/dataset/weatherbench_healpix.py
+++ external/custom_hermes/dataset/weatherbench_healpix.py
@@ -231,7 +231,8 @@ class WeatherbenchHealpix(Dataset):
 
             print("Projecting to HEALPix...")
             hp_maps, _, _ = sphar_to_healpix(sphar_coeffs, nside)
-            healpix_vals = torch.from_numpy(hp_maps).float()
+            # this technically does not need cuda, but i cant get cpus only on explorer, so sorry
+            healpix_vals = torch.from_numpy(hp_maps).float().cuda()
 
             if split == "train":
                 x_mean = healpix_vals.mean().item()
@@ -241,7 +242,7 @@ class WeatherbenchHealpix(Dataset):
                 print(f"mean={x_mean:.4f}, std={x_std:.4f}")
 
             torch.save({
-                "healpix_vals": healpix_vals,
+                "healpix_vals": healpix_vals.cpu(),
             }, os.path.join(save_dir, f"{split}.pt"))
             print(
                 f"  Saved to {save_dir}/{split}.pt  shape={healpix_vals.shape}")
