03af1aed705946ab0430202a3dde17043120e1df
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-03-29 00:14:08 -0400

    luisa did not bother writing a commit message

--- data_preprocessing/weatherbench_healpix/parallel_batch.sh
+++ data_preprocessing/weatherbench_healpix/parallel_batch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --time=07:59:59
-#SBATCH --job-name=[prep]
+#SBATCH -J preprocess_weatherbench
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task=4 
 #SBATCH --mem=32GB
-#SBATCH --partition=gpu
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:v100-sxm2:1
-#SBATCH --output=slurm/%j.out
-#SBATCH --error=slurm/%j.err
-
+#SBATCH --time=07:59:59
+#SBATCH --partition=short
+#SBATCH -o slurm/%j.out
+#SBATCH -e slurm/%j.err
 
 module load python/3.13.5
 
