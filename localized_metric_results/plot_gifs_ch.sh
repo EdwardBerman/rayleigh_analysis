bb6b1ba26959c8b14cc0cdfac6e1ef311a55e299
EdwardBerman <10emb29@gmail.com>
2026-05-04 23:42:44 -0400

    oops

--- localized_metric_results/plot_gifs_ch.sh
+++
@@ -1,8 +0,0 @@
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=gem_cnn  model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_GemCNN_model.pt  
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=EMAN  model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_EMAN_model.pt
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=uni  model_save_path=model_checkpoints/Cahn-Hilliard_Uni_seed1_model.pt   
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=egnn model_save_path=model_checkpoints/Cahn-Hilliard_EGNN_h32_model.pt
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=gcn  model_save_path=model_checkpoints/Cahn-Hilliard_GCN_seed1_model.pt
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=mpnn  model_save_path=model_checkpoints/Cahn-Hilliard_MPNN_seed1_model.pt
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=hermes  model_save_path=external/hermes/pretrained_checkpoints/Cahn-Hilliard_Hermes_model.pt
-python3 -m external.custom_hermes.eval_rollout_local_metric  dataset=cahn_hilliard backbone=transformer  model_save_path=model_checkpoints/Cahn-Hilliard_transformer_seed1_model.pt
